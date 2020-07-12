e162825baff1def02520fcbda9b2f6f83251107a
MADCAT <orioncloud2020@gmail.com>
2020-07-12 22:19:29 +0300

    infinite loop solved for given 0

--- Assignment4 _Is_it _a_Prime_Number.py
+++ Assignment4 _Is_it _a_Prime_Number.py
@@ -6,11 +6,14 @@ Created on Sat Jul 11 14:17:49 2020
 """
 while True:
     try:
-        x = int(input("enter an integer or enter a letter to exit:"))
-    
+        x = abs(int(input("enter an integer or enter a letter to exit:")))
+        if x == 2 :        
+            print(f"{x} can divided by only 1 and itself, so it is a prime number") 
+        elif x == 0 :
+            print ("0 is not a prime number")
         i = 1
         cont = True
-        while i != x-1:
+        while x != 0 and i != x-1:
             i += 1 
             if x % i == 0:
                 print(f"{x} can divided by {i}, so it is not a prime number")
@@ -22,8 +25,7 @@ while True:
                 if i == x-1 and cont:
                     print(f"{x} can divided by only 1 and itself,\
 so it is a prime number")
-        if x == 2 :        
-            print(f"{x} can divided by only 1 and itself, so it is a prime number")  # only for num = 2
+         # only for num = 2
         
     except ValueError:
         print("güle güle")
