205e7bc75bf5e19238223ff4fdd4fdb5e5bc0d5b
MADCAT <orioncloud2020@gmail.com>
2020-07-12 20:39:24 +0300

    changes to make multiple entries available

--- Assignment4 _Is_it _a_Prime_Number.py
+++ Assignment4 _Is_it _a_Prime_Number.py
@@ -4,21 +4,24 @@ Created on Sat Jul 11 14:17:49 2020
 
 @author: MADCAT
 """
-
-
-x = int(input("enter an integer:"))
-
-i = 1
-cont = True
-while i != x:
-   i += 1 
-   if x-1 % i == 0:
-       print(f"{x} can divided by {i}, so it is not a prime number")
-       cont = False 
-   elif x == 1:
-        print(f"{x} can divided by only itself, so it is a prime number")
-        break
-   else :
-       if i == x-1 and cont:
-           print(f"{x} can divided by only 1 and itself, so it is a prime number")
+while True:
+    try:
+        x = int(input("enter an integer or enter a letter to exit:"))
     
+        i = 1
+        cont = True
+        while i != x-1:
+            i += 1 
+            if x % i == 0:
+                print(f"{x} can divided by {i}, so it is not a prime number")
+                cont = False 
+            elif x == 1:
+                print(f"{x} can divided by only itself, so it is a prime number")
+                break
+            else :
+                if i == x-1 and cont:
+                    print(f"{x} can divided by only 1 and itself,\
+so it is a prime number")
+    except ValueError:
+        print("güle güle")
+        break
