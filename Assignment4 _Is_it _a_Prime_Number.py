eb43d301fc87534578665e7476434828ed5da52c
E2060Fatih <65829567+E2060Fatih@users.noreply.github.com>
2020-07-12 20:19:34 +0300

    Update Assignment4 _Is_it _a_Prime_Number.py

--- Assignment4 _Is_it _a_Prime_Number.py
+++ Assignment4 _Is_it _a_Prime_Number.py
@@ -9,16 +9,16 @@ Created on Sat Jul 11 14:17:49 2020
 x = int(input("enter an integer:"))
 
 i = 1
-cont = 0
+cont = True
 while i != x:
    i += 1 
    if x-1 % i == 0:
        print(f"{x} can divided by {i}, so it is not a prime number")
-       cont += 1 
+       cont = False 
    elif x == 1:
         print(f"{x} can divided by only itself, so it is a prime number")
         break
    else :
-       if i == x-1 and cont == 0:
+       if i == x-1 and cont:
            print(f"{x} can divided by only 1 and itself, so it is a prime number")
     
